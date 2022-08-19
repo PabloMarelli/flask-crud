25cda21f71f4949f85dde84a3a0c4fa9a17544a9
Pablo Marelli <pablomarelli2@gmail.com>
2022-08-18 21:25:07 -0300

    Adds views functions

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, url_for, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
@@ -13,16 +13,33 @@ db  = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
-    date_created = db.Column(db.Date, default=datetime.utcnow)
+    date_created = db.Column(db.DateTime, default=datetime.now)
 
     def __repr__(self):
         return '<Task %r>' % self.id
 
 
 # Routing
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo(content=task_content)
+
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an issue adding yout task'
+
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all() 
+        return render_template('index.html', tasks=tasks)
+
+
+# @app.route('/delete/')
+    
 
 # Running server
 if __name__ == '__main__':
